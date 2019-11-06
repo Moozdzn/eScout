e2c2832173412135f12eb6d6fcda586fb6e28dce
!null <franciscordeiro99@gmail.com>
2019-11-06 15:54:10 +0000

    Upload video first mockup - no css

--- utils/database_conn.js
+++ utils/database_conn.js
@@ -1,11 +1,5 @@
 var mysql = require('mysql');
 
-// DBPASS must be defined with the database password
-// Powershell: $env:DBPASS="password"
-// CMD (windows): set DBPASS="password"
-// Linux and iOS: export DBPASS="password"
-// Heroku: Go to Settings and then to config vars
-
 var pool  = mysql.createPool({
     connectionLimit : 10,
     host     : 'remotemysql.com',
