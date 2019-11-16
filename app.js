8e00261e3d90943bb206719d0b3759efc6d262b2
!null <franciscordeiro99@gmail.com>
2019-11-16 19:53:43 +0000

    upload file

--- app.js
+++ app.js
@@ -8,6 +8,23 @@ var guestRouter = require('./routes/guestRoute');
 
 var app = express();
 
+var fs = require('fs');
+var busboy = require('connect-busboy');
+
+app.use(busboy()); 
+
+app.post('/fileupload', function(req, res) {
+    var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+        console.log("Uploading: " + filename); 
+        fstream = fs.createWriteStream(__dirname + '/uploads/' + filename);
+        file.pipe(fstream);
+        fstream.on('close', function () {
+            res.redirect('back');
+        });
+    });
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
--- routes/guestRoute.js
+++ routes/guestRoute.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'eScout' });
 });
+
 
 /* GET home page from profile. 
 router.get('/profile', function(req, res, next) {
