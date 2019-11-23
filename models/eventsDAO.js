1fe5b588df503d13153e00f7b0f1899d6516351e
!null <franciscordeiro99@gmail.com>
2019-11-23 14:54:02 +0000

    events display data

--- models/eventsDAO.js
+++ models/eventsDAO.js
@@ -2,13 +2,12 @@ var pool = require("./conn").pool;
 
 
 module.exports.getEvents = function (cb, next) {
-    console.log("chupa cabra");
     pool.getConnection(function (err, conn) {
         if (err) {
             cb(err, { code: 500, status: "Error connecting to database." })
             return;
         }
-        conn.query("SELECT Event.name, Event.description, Event.date, EventLocation.latitude, EventLocation.longitude, EventLocation.name FROM Event INNER JOIN EventLocation ON Event.locationID = EventLocation.locationID", function (err, results) {
+        conn.query("SELECT Event.eventName, Event.eventDescription, Event.eventDate, EventLocation.latitude, EventLocation.longitude, EventLocation.eventlocationName FROM Event INNER JOIN EventLocation ON Event.locationID = EventLocation.locationID", function (err, results) {
             conn.release();
             if (err) {
                 cb(err, { code: 500, status: "Error in a database query" })
--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -1,7 +1,6 @@
 
 
 window.onload = function () {
-    console.log("batata");
     var eventslist = document.getElementById("eventslist");
     $.ajax({
         url: '/api/events',
@@ -10,9 +9,11 @@ window.onload = function () {
         success: function (res, status) {
             if (res.err) return;
             var html = "";
-            for (i in res)
-                html += '<div>' + res[i].name + '</div>';
-
+            for (i in res){
+                html += '<div class="col-lg-4 col-md-6 mb-4"><div class="card h-100"><div class="card-body"> <h4 class="card-title"><a onclick="showMarker(1)">'+res[i].eventName+'</a></h4><p class="card-text">'+res[i].eventDescription+'</p></div></div></div>';
+              
+                 
+            }
             eventslist.innerHTML = html;
         }
 
