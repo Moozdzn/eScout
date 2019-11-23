d7ae166049219868709d274169200d9b8de0504b
!null <franciscordeiro99@gmail.com>
2019-11-23 15:06:10 +0000

    minor changes

--- models/eventsDAO.js
+++ models/eventsDAO.js
@@ -7,7 +7,7 @@ module.exports.getEvents = function (cb, next) {
             cb(err, { code: 500, status: "Error connecting to database." })
             return;
         }
-        conn.query("SELECT Event.eventName, Event.eventDescription, Event.eventDate, EventLocation.latitude, EventLocation.longitude, EventLocation.eventlocationName FROM Event INNER JOIN EventLocation ON Event.locationID = EventLocation.locationID", function (err, results) {
+        conn.query("SELECT * FROM Event INNER JOIN EventLocation ON Event.locationID = EventLocation.locationID", function (err, results) {
             conn.release();
             if (err) {
                 cb(err, { code: 500, status: "Error in a database query" })
--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -9,11 +9,9 @@ window.onload = function () {
         success: function (res, status) {
             if (res.err) return;
             var html = "";
-            for (i in res){
-                html += '<div class="col-lg-4 col-md-6 mb-4"><div class="card h-100"><div class="card-body"> <h4 class="card-title"><a onclick="showMarker(1)">'+res[i].eventName+'</a></h4><p class="card-text">'+res[i].eventDescription+'</p></div></div></div>';
-              
-                 
-            }
+            for (i in res)
+                html += '<div class="col-lg-4 col-md-6 mb-4"><div class="card h-100"><div class="card-body"> <h4 class="card-title"><a onclick="showMarker('+res[i].eventID+')">'+res[i].eventName+'</a></h4><p class="card-text">'+res[i].eventDescription+'</p></div></div></div>';
+                
             eventslist.innerHTML = html;
         }
 
