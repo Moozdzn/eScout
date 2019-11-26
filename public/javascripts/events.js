bb501a27a47a839bc7478aa8f3753cb3b1cdbe4d
!null <franciscordeiro99@gmail.com>
2019-11-26 09:09:46 +0000

    events href hotfix

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -18,7 +18,7 @@ window.onload = function () {
             if (res.err) return;
             var html = "";
             for (i in res)
-                html += '<div class="col-lg-4 col-md-6 mb-4" onclick="showMarker('+res[i].eventID+')"><div class="card h-100"><div class="card-body"> <h4 class="card-title"><a >'+res[i].eventName+'</a></h4><p class="card-text">'+res[i].eventDescription+'</p></div></div></div>';
+                html += '<div class="col-lg-4 col-md-6 mb-4" onclick="showMarker('+res[i].eventID+')"><div class="card h-100"><div class="card-body"> <h4 class="card-title"><a href="#">'+res[i].eventName+'</a></h4><p class="card-text">'+res[i].eventDescription+'</p></div></div></div>';
                 
             eventslist.innerHTML = html;
         }
