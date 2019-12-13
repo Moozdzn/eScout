3ac7140964b74c608d7e9946a7aa6aa7e652cce5
Moozdzn <moozdzn@gmail.com>
2019-12-13 13:49:16 +0000

    drive upload hotfix

---
+++ public/javascripts/uploadVideo.js
@@ -0,0 +1,20 @@
+
+$('#sendVideo').click(function (evt) {
+    evt.preventDefault();
+    console.log('test1')
+    var data = new FormData($("#formFile")[0]);
+    data.append('UserID', sessionStorage.userID)
+    $.ajax({
+        url: "/api/users/videoupload",
+        method : "post",
+        data : data,
+        processData: false,
+contentType: false,
+        success: function(res, status){ 
+         console.log(res)
+        }
+        
+        , error : function() { alert(JSON.stringify('error')); }
+        
+        });
+})
