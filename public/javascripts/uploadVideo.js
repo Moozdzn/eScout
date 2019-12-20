f83a21b3ac93e56e0dd2d6d5f95b805e2d53f335
Moozdzn <moozdzn@gmail.com>
2019-12-20 11:14:02 +0000

    WIP Streamable Video Upload from server

--- public/javascripts/uploadVideo.js
+++ public/javascripts/uploadVideo.js
@@ -3,15 +3,13 @@ $('#sendVideo').click(function (evt) {
     evt.preventDefault();
     
     var data = new FormData($("#formFile")[0]);
+    console.log(data)
     //data.append('UserID', sessionStorage.userID)
     /*curl https://api.streamable.com/upload
         -u email:password
         -F file=@myvideo.mp4*/
     $.ajax({
-        url: "https://api.streamable.com/upload",
-        beforeSend: function(xhr) { 
-            xhr.setRequestHeader("Authorization", "Basic " + btoa("escout.app@gmail.com:escout123")); 
-          },
+        url: "/api/users/videoupload",
         method : "post",
         data : data,
         processData: false,
@@ -24,3 +22,21 @@ $('#sendVideo').click(function (evt) {
         
         });
 })
+/*
+$.ajax({
+        url: "https://api.streamable.com/upload",
+        beforeSend: function(xhr) { 
+            xhr.setRequestHeader("Authorization", "Basic ZXNjb3V0LmFwcEBnbWFpbC5jb206ZXNjb3V0MTIz"); 
+          },
+        method : "post",
+        data : data,
+        processData: false,
+        contentType: false,
+        success: function(res, status){ 
+         console.log(res)
+        }
+        
+        , error : function(res) {console.log(res) }
+        
+        });
+*/
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,6 +3,9 @@ const gdrive = require('./gdrive');
 var profileDAO = require('../models/usersDAO');
 var videoDAO = require('../models/videoDAO');
 var router = express.Router();
+const axios = require('axios');
+var FormData = require('form-data');
+const fs = require("fs");
 
 
 router.get("/profile/:id", function(req,res,next){
@@ -54,7 +57,7 @@ router.get("/profile/:id/videos", function(req,res,next){
     }, next)
 });
 
-router.post("/videoupload", function(req,res){
+/* router.post("/videoupload", function(req,res){
     try {
         console.log(req.body.VideoTitle)
         if(!req.files) {
@@ -64,7 +67,7 @@ router.post("/videoupload", function(req,res){
             });
         } else {
             let video = req.files.video;
-            console.log(video);
+            
             video.mv('./uploads/' + video.name);
             try {
               gdrive.videoUpload(video.name, './uploads/' + video.name, (id) => {
@@ -92,7 +95,7 @@ router.post("/videoupload", function(req,res){
         res.status(500).send(err);
         console.log(err);
     }
-  });
+  }); */
 
   router.post("/:logUser/messages/:contactID/new", function(req,res,next){
     var user = req.params.logUser;
@@ -108,6 +111,35 @@ router.post("/videoupload", function(req,res){
         }
         res.status(result.code).send(result.data);
     }, next)
+
+});
+
+router.post("/videoupload", function(req,res,next){
+    console.log(req);
+    console.log('teste');
+    let video = req.files.video;
+    video.mv('./uploads/' + video.name);
+    var filePath = './uploads/' + video.name;
+    
+    console.log(video);
+    try {
+    var bodyFormData = new FormData();
+    bodyFormData.append('file',fs.createReadStream(filePath));
+    console.log('teste')
+} catch (err)  {
+    console.log(err)
+}
+    console.log(bodyFormData);
+    axios({
+        url:"https://api.streamable.com/upload",
+        headers: {'Content-Type':'multipart/form-data',"Authorization": "Basic ZXNjb3V0LmFwcEBnbWFpbC5jb206ZXNjb3V0MTIz"},
+        method: 'post',
+        data : bodyFormData,
+        maxContentLength: Infinity,
+        maxBodyLength: Infinity
+    }).then(function(res, status){ 
+        console.log(res+'work')
+    }).catch(function(res) {console.log(res+'no work') })
 
 });
 
