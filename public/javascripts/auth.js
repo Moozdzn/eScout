7f5b8c839991e8642858232b94464a81e3546f9f
!null <franciscordeiro99@gmail.com>
2019-12-28 21:16:21 +0000

    create event only to Event Organizers

--- public/javascripts/auth.js
+++ public/javascripts/auth.js
@@ -1,27 +1,26 @@
 
-  $('#confirm').submit(function (evt) {
-    evt.preventDefault();
-    $.ajax({
-        url: "/api/auth",
-        method : "post",
-        contentType : "application/json",
-        data : JSON.stringify({ 
-                username: $("#inputUsername").val(), 
-                password: $("#inputPassword").val(),  
-            }),
-        
-        success: function(res, status){ 
-          sessionStorage.userID = parseInt(Object.values(res[0]));;
-          window.location.href = '/';
-          
-          //
-        }
-        
-        , error : function() { alert(JSON.stringify('error')); }
-        
-        });
+$('#confirm').submit(function (evt) {
+  evt.preventDefault();
+  $.ajax({
+    url: "/api/auth",
+    method: "post",
+    contentType: "application/json",
+    data: JSON.stringify({
+      username: $("#inputUsername").val(),
+      password: $("#inputPassword").val(),
+    }),
+
+    success: function (res, status) {
+      sessionStorage.userID = res.userID;
+      sessionStorage.userType = res.userType;
+      window.location.href = '/';
+    }
+
+    , error: function () { alert(JSON.stringify('error')); }
+
+  });
 })
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
 });
