b49dbb4304b511ae08f51232d04dd5bec7c44647
!null <franciscordeiro99@gmail.com>
2020-01-30 17:26:31 +0000

    removed register

--- models/authDAO.js
+++ models/authDAO.js
@@ -19,23 +19,3 @@ module.exports.verifyUser = function (data,cb, next) {
 
     })
 }
-
-module.exports.userList = function (cb, next) {
-    
-    pool.getConnection(function (err, conn) {
-        if (err) {
-            cb(err, { code: 500, status: "Error connecting to database." })
-            return;
-        }
-        conn.query("SELECT username FROM User;", function (err, results) {
-            conn.release();
-            if (err) {
-                cb(err, { code: 500, status: "Error in a database query" })
-                return;
-            }
-            cb(false, { code: 200, status: "ok", data: results })
-        })
-
-
-    })
-}
